c7a333f314e34679bf8405ad8bb23f17b679e435
popcornmix <popcornmix@gmail.com>
2013-06-27 01:25:57 +0100

    [rbp/omxplayer] Do we need discontinuity handling?
    So far I've not seen what this is needed for and it does cause problems for some files.

--- xbmc/cores/omxplayer/OMXPlayer.cpp
+++ xbmc/cores/omxplayer/OMXPlayer.cpp
@@ -2395,6 +2395,7 @@ static void UpdateLimits(double& minimum, double& maximum, double dts)
 
 void COMXPlayer::CheckContinuity(COMXCurrentStream& current, DemuxPacket* pPacket)
 {
+return;
   if (m_playSpeed < DVD_PLAYSPEED_PAUSE)
     return;
 
